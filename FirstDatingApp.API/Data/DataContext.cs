03b21fe770535a9edd2a9f53b444cbcb33ce1579
Lakshmanan <Santana.Lakshmanan@lamresearch.com>
2020-05-13 14:14:48 -0700

    Initial Commit

---
+++ FirstDatingApp.API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+//using Microsoft.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+    public DbSet<Value> Values {get; set;}
+}
---
+++ FirstDatingApp.API/Models/Value.cs
@@ -0,0 +1,6 @@
+public class Value
+{
+public int Id { get; set; }
+public string Name { get; set; }
+
+} 
